1b39fa31857133b81dc4576713aa5bc93d17dc46
aldoht <aldofernandoht@gmail.com>
2025-03-23 23:52:52 -0600

    complete(acts): finish act9

--- Actividades/Actividad 9/Actividad9.py
+++ Actividades/Actividad 9/Actividad9.py
@@ -1,17 +1,13 @@
 import numpy as np
 import pandas as pd
-import seaborn as sb
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-from matplotlib import cm
 plt.rcParams['figure.figsize'] = (16, 9)
 plt.style.use('ggplot')
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
 data = pd.read_csv('./articulos_ml.csv')
-data.drop(['Title', 'url', 'Elapsed days'], axis=1).hist()
-plt.show()
+data.drop(['Title', 'url', 'Elapsed days'], axis=1)
 
 filtered_data = data[(data['Word count'] <= 3500) & data['# Shares'] <= 80000]
 colors = ['orange', 'blue']
@@ -24,5 +20,32 @@ for index, row in filtered_data.iterrows():
         assign.append(colors[0])
     else:
         assign.append(colors[1])
-plt.scatter(f1, f2, c=assign, s=size[0])
+
+col_sum = (filtered_data['# of Links'] + filtered_data['# of comments'].fillna(0) + filtered_data['# Images video'])
+dataX2 = pd.DataFrame()
+dataX2['Word count'] = filtered_data['Word count']
+dataX2['suma'] = col_sum
+XY_train = np.array(dataX2)
+z_train = filtered_data['# Shares'].values
+regr2 = linear_model.LinearRegression()
+regr2.fit(XY_train, z_train)
+z_pred = regr2.predict(XY_train)
+print(f'Coefficients: {regr2.coef_}. MSE: {round(mean_squared_error(z_train, z_pred), 2)}. Variance score: {round(r2_score(z_train, z_pred), 2)}.')
+
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+xx, yy = np.meshgrid(np.linspace(0, 3500, num=10), np.linspace(0, 60, num=10))
+nuevoX = (regr2.coef_[0] * xx)
+nuevoY = (regr2.coef_[1] * yy)
+z = (nuevoX + nuevoY + regr2.intercept_)
+ax.plot_surface(xx, yy, z, alpha=0.2, cmap='hot')
+ax.scatter(XY_train[:, 0], XY_train[:, 1], z_train, c='blue', s=30)
+ax.scatter(XY_train[:, 0], XY_train[:, 1], z_pred, c='red', s=40)
+ax.view_init(elev=30, azim=65)
+ax.set_xlabel('Cantidad de palabras')
+ax.set_ylabel('Cantidad de enlaces, comentarios e imágenes')
+ax.set_zlabel('Compartidos en redes sociales')
+ax.set_title('Regresión lineal con múltiples variables')
 plt.show()
+
+print(regr2.predict([[2000, 10+4+6]]))
